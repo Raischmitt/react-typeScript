20e450d14535d629596ce30dddece34996b1edbe
Raissa Schmitt <raissajoaquim29@gmail.com>
2025-10-23 19:38:12 -0300

    separando texto da page todos

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,11 +1,17 @@
 import Todos from './components/Todos';
+import Todo from './models/todo';
 
 import './App.css';
 
 function App() {
+  const todos = [
+    new Todo('Learn React'),
+    new Todo('Learn Typescript')
+  ];
+
   return (
     <div>
-      <Todos items={['Learn React', 'Learn typescript']} />
+      <Todos items={todos} />
     </div>
   );
 }
