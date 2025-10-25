888a21e9166304a39c707e6132b6282c715c9ff1
Raissa Schmitt <raissajoaquim29@gmail.com>
2025-10-24 22:39:13 -0300

    usando useState com o typescript

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,16 +1,25 @@
+import { useState } from 'react';
+
 import Todos from './components/Todos';
 import Todo from './models/todo';
+import NewTodo from './components/NewTodo';
 
 import './App.css';
 
 function App() {
-  const todos = [
-    new Todo('Learn React'),
-    new Todo('Learn Typescript')
-  ];
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+  const addTdoHandler = (todoText: string) => {
+    const newTodo = new Todo(todoText);
+
+    setTodos((prevTodos) => {
+      return prevTodos.concat(newTodo);
+    });
+  };
 
   return (
     <div>
+      <NewTodo onAddTodo={addTdoHandler}/>
       <Todos items={todos} />
     </div>
   );
